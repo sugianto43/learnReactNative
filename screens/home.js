1ca512b1701ba99b17ebfcc9c2e579130b29a2a3
sugianto43 <antho9922@gmail.com>
2021-09-19 18:40:01 +0700

    create project base

---
+++ screens/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { globalStyles } from '../styles/global'
+
+const Home = () => {
+  return (
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.titleText}>Home screen</Text>
+    </View>
+  )
+}
+
+export default Home
---
+++ screens/reviewDetails.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { globalStyles } from '../styles/global'
+
+const ReviewDetails = () => {
+  return (
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.titleText}>Review Details screen</Text>
+    </View>
+  )
+}
+
+export default ReviewDetails
